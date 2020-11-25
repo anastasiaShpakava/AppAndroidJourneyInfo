337aa9662fd70cc25d126aa45fbfdc664e576063
anastasiaShpakava <shpakavaa@gmail.com>
2020-11-25 13:24:29 +0300

    added code fo sign up and sign in with Firebase using

--- app/src/main/java/com/company/journeyInfo/MainActivity.java
+++ app/src/main/java/com/company/journeyInfo/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.company.journeyInfo.ui.MainPageFragment;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragmentContainer, new MainPageFragment())
+                    .commit();
+        }
     }
 }
